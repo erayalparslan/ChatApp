675f474c4a8cfd9bbe299566d176d2414035e2d0
Eray Alparslan <erayalparslan@yahoo.com>
2020-01-11 16:11:51 +0300

    first commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Eray on 11.01.2020.
+//  Copyright © 2020 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
